8d18666b11866769f3736afc1dd20e570dcf9a71
Rodrigo <rods_araujo@hotmail.com>
2024-06-18 23:02:30 -0300

    primeiro commit

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,14 @@
+import Cabecalho from "components/Cabecalho";
+import Footer from "components/Footer";
+//import styles from "./Inicio.module.css";
+
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Footer />
+    </>
+  );
+}
+
+export default Inicio;
